b33326c8cf4a33042571a83f1c9ee7f887e381b0
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-09-26 23:09:41 +0300

    Відмалював 1 сторінку популярних фільмів

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import "./js/getRefs"
+import NewApiService from './js/api/apiFn';
+import allGenres from './genres.json';
+
+const refs = {
+  popularFilmsList: document.querySelector('.container__main'),
+};
+
+const filmsFavour = new NewApiService();
+filmsFavour.fetchArticles().then(r => createPopularFilmsMarkup(r));
+
+function createPopularFilmsMarkup(films) {
+  const markup = films
+    .map(film => {
+      const genres = film.genre_ids
+        .map(id => {
+          for (genre of allGenres) {
+            if (id === genre.id) {
+              return genre.name;
+            }
+          }
+        })
+        .join(', ');
+      return `<li class="card__item" id=${film.id}>
+        <a class="card__link" id="429473" href="#">
+          <img
+            src="https://image.tmdb.org/t/p/w500/${film.poster_path}"
+            alt="${film.original_title}"
+            class="card__poster"
+          />
+
+          <h2 class="card__title">${film.original_title}</h2>
+          <div class="card__wrap">
+            <p class="card__description">${genres} | ${film.release_date.slice(
+        0,
+        4
+      )}</p>
+            <p class="card__rating">${film.vote_average.toFixed(1)}</p>
+          </div>
+        </a>
+      </li>`;
+    })
+    .join('');
+  refs.popularFilmsList.innerHTML = markup;
+}
--- src/js/api/apiFn.js
+++ src/js/api/apiFn.js
@@ -1,1 +1,35 @@
-// 8fa17eefa9c2b424e1a30217c39bc412
+export default class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    // this.totalPages = null;
+  }
+
+  async fetchArticles() {
+    const API_KEY = '8fa17eefa9c2b424e1a30217c39bc412';
+    const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&page=${this.page}`;
+
+    const r = await fetch(url);
+    const data = await r.json();
+    const films = await this.getArrayOfFilms(data);
+    return films;
+  }
+
+  getArrayOfFilms(r) {
+    // console.log(r.results);
+    return r.results;
+  }
+
+  //   resetPage() {
+  //     this.page = 1;
+  //     this.totalPages = null;
+  //   }
+
+  //   get query() {
+  //     return this.searchQuery;
+  //   }
+
+  //   set query(newQuery) {
+  //     this.searchQuery = newQuery;
+  //   }
+}
