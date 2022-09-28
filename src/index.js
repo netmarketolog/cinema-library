d675adadeccf6ca7eaaaee35ee80e9cd5787f1f7
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-09-28 16:54:07 +0300

    Local Storage
    Теж саме

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import "./js/getRefs"
+<<<<<<< Updated upstream
 import "./js/local-storage/addMovie";
+=======
+import "./js/local-storage/addMovie"
+>>>>>>> Stashed changes
--- src/js/local-storage/addMovie.js
+++ src/js/local-storage/addMovie.js
@@ -3,6 +3,7 @@
 import throttle from 'lodash.throttle';
 
 // Константи
+<<<<<<< Updated upstream
 const KEY_QUEUE = 'queue';
 const KEY_WATCHED = 'watched';
 let queueList = [];
@@ -11,6 +12,20 @@ let watchedList = [];
 // Шлях до кнопок
 const addToQueueBtn = document.querySelector("[data-addToQueue]");
 const addToWatchedBtn = document.querySelector("[data-addToWatched]");
+=======
+const STORAGE_KEY_QUEUE = 'storage-queue';
+const STORAGE_KEY_WATCHED = 'storage-watched';
+const queueData = {};
+const watchedData = {};
+queueListData();
+viewListData();
+
+// Шлях до кнопок
+const addToQueueBtn = document.querySelector(".addToQueueBtn");
+const addToWatchedBtn = document.querySelector(".addToWatchedBtn");
+const queueBtn = document.querySelector(".queueBtn");
+const watchedBtn = document.querySelector(".watchedBtn");
+>>>>>>> Stashed changes
 
 // Слухачі подій
 addToQueueBtn.addEventListener('click', throttle(onQueueBtn, 500));
@@ -18,6 +33,7 @@ addToWatchedBtn.addEventListener('click', throttle(onWatchedBtn, 500));
 
 // Функція для роботи зі сховищем
 // Функція додавання фільмів до черги
+<<<<<<< Updated upstream
 function onQueueBtn () {
     let id = addToQueueBtn.dataset.action;
     console.log(id);
@@ -60,3 +76,54 @@ function onWatchedBtn () {
     localStorage.setItem(KEY_WATCHED, JSON.stringify(watchedList));
 }
 
+=======
+function onQueueBtn (e) {
+    // modalData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY_QUEUE, JSON.stringify(queueData));
+}
+// Функція читання фільмів в черзі
+function queueListData() {
+    const form = document.querySelector(".feedback-form");
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    const formData = JSON.parse(saveMessage);
+    try {
+        if(formData.email) {   
+            form.email.value = formData.email;
+        }
+    } catch (err) {
+      
+      }
+      try {
+        if(formData.message) {   
+            form.message.value = formData.message;
+        }
+    } catch (err) {
+      
+      }
+}
+// Функція додавання переглянутих фільмів
+function onWatchedBtn (e) {
+    // formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY_WATCHED, JSON.stringify(watchedData));
+}
+// Функція читання переглянутих фільмів
+function viewListData() {
+    const form = document.querySelector(".feedback-form");
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    const formData = JSON.parse(saveMessage);
+    try {
+        if(formData.email) {   
+            form.email.value = formData.email;
+        }
+    } catch (err) {
+      
+      }
+      try {
+        if(formData.message) {   
+            form.message.value = formData.message;
+        }
+    } catch (err) {
+      
+      }
+}
+>>>>>>> Stashed changes
