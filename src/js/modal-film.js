fcc5ab3beed4cce64608ae9e8b8dba7cae71112b
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-09-29 15:19:09 +0300

    open_modal_work
    with rendering

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -6,14 +6,13 @@ const refs = getRefs();
 
 refs.popularFilmsList.addEventListener('click', e => {
   e.preventDefault();
-  console.log(e.target);
-  console.log(e.elements);
   const isCardMovie = e.target.closest('.card__item');
+  // console.log(isCardMovie.id);
   if (!isCardMovie) {
     return;
   }
 
-  openModal();
+  openModal(isCardMovie.id);
 
   refs.popularFilmsList.addEventListener('keydown', e => {
     if (e.key === 'Escape') {
@@ -30,8 +29,8 @@ async function fetchDescr(filmId) {
   return descriptionFilm;
 }
 
-function openModal() {
-  fetchDescr().then(film => {
+function openModal(movie) {
+  fetchDescr(movie).then(film => {
     console.log(film);
     refs.modalRendEl.innerHTML = `<div class="film__poster" id=${film.id}>
         <img
@@ -42,44 +41,39 @@ function openModal() {
         />
       </div>
       <div class="film__info">
-        <h2 class="film__title">A FISTFUL OF LEAD</h2>
+        <h2 class="film__title">${film.original_title}</h2>
         <div class="film__description">
           <table>
             <tbody>
               <tr>
                 <th class="film__attribute">Vote / Votes</th>
                 <td class="film__att-value">
-                  <span class="film__vote">7.3</span> /
-                  <span class="film__votes">1260</span>
+                  <span class="film__vote">${film.vote_average.toFixed(
+                    1
+                  )}</span> /
+                  <span class="film__votes">${film.vote_count}</span>
                 </td>
               </tr>
               <tr>
                 <th class="film__attribute">Popularity</th>
-                <td class="film__att-value">100.2</td>
+                <td class="film__att-value">${film.popularity.toFixed(1)}</td>
               </tr>
               <tr>
                 <th class="film__attribute">Original Title</th>
-                <td class="film__att-value">A FISTFUL OF LEAD</td>
+                <td class="film__att-value">${film.original_title}</td>
               </tr>
               <tr>
                 <th class="film__attribute">Genre</th>
-                <td class="film__att-value">Western</td>
+                <td class="film__att-value">${film.genres
+                  .map(genre => genre.name)
+                  .join(', ')}</td>
               </tr>
             </tbody>
           </table>
         </div>
         <div class="film__about">
           <h3 class="film__about-title">About</h3>
-          <p class="film__text">
-            Four of the West’s most infamous outlaws assemble to steal a huge
-            stash of gold from the most corrupt settlement of the gold rush
-            towns. But not all goes to plan one is killed and the other three
-            escapes with bags of gold hide out in the abandoned gold mine where
-            they happen across another gang of three – who themselves were
-            planning to hit the very same bank! As tensions rise, things go from
-            bad to worse as they realise the bags of gold are filled with
-            lead... they’ve been double crossed – but by who and how?
-          </p>
+          <p class="film__text">${film.overview}</p>
         </div>`;
   });
 
