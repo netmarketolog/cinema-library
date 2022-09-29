052a8c2efc0310e1d7d70518d6338c1cd8c3e28f
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-09-29 11:18:36 +0300

    add pagination function

--- src/js/api/fetchPopularFilms.js
+++ src/js/api/fetchPopularFilms.js
@@ -1,5 +1,5 @@
 import NewApiService from './apiFn';
-import Pagination from 'tui-pagination';
+import addPagination from './pagination';
 import createPopularFilmsMarkup from './renderMarkup';
 import getRefs from '../getRefs';
 
@@ -8,25 +8,12 @@ export default function fetchPopularFilms() {
   refs.spinner.classList.remove('is-hidden');
   const filmsPopular = new NewApiService();
   const url = '3/trending/movie/week';
-  console.log(url);
+
   filmsPopular
-    .fetchArticles(url, false)
+    .fetchArticles(url)
     .then(r => {
       createPopularFilmsMarkup(r);
-      const pagination = new Pagination('pagination', {
-        totalItems: filmsPopular.totalFilms,
-        itemsPerPage: 20,
-        centerAlign: true,
-        visiblePages: 5,
-      });
-      pagination.on('beforeMove', function (e) {
-        refs.spinner.classList.remove('is-hidden');
-        filmsPopular.page = e.page;
-        filmsPopular.fetchArticles(url).then(r => {
-          createPopularFilmsMarkup(r);
-          refs.spinner.classList.add('is-hidden');
-        });
-      });
+      addPagination(filmsPopular, url);
       refs.spinner.classList.add('is-hidden');
     })
     .catch(console.log);
