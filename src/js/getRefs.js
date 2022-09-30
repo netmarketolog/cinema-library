a4327581a483eb1309f4558e37547ee6ff7343b3
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-09-30 13:53:53 +0300

    fixed modal-team

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -8,13 +8,12 @@ export default function getRefs() {
     queueBtn: document.querySelector('[data-action="queue"]'),
     modalRendEl: document.querySelector('.film__container'),
     body: document.querySelector('body'),
-    modalTeamEl: document.querySelector ('.backdrop-team'),
-    modalTeamOpenBtn: document.querySelector ('[data-action="team-open-modal"]'),
-    modalTeamCloseBtn: document.querySelector ('[data-action="team-close-modal"]')
     modalTeamEl: document.querySelector('.backdrop-team'),
-    modalTeamOpenBtn: document.querySelector('.footer__team'),
+    modalTeamOpenBtn: document.querySelector('[data-action="team-open-modal"]'),
+    modalTeamCloseBtn: document.querySelector(
+      '[data-action="team-close-modal"]'
+    ),
     paginationEl: document.querySelector('.pagination ul'),
-
   };
   return refs;
 }
--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,32 +1,29 @@
 import getRefs from './getRefs';
 
-const refs = getRefs()
+const refs = getRefs();
 
-refs.modalTeamOpenBtn.addEventListener ('click', openModalTeam)
-refs.modalTeamCloseBtn.addEventListener ('click', closeModalTeam)
-refs.modalTeamEl.addEventListener ('click', onBackdropClick)
+refs.modalTeamOpenBtn.addEventListener('click', openModalTeam);
+refs.modalTeamCloseBtn.addEventListener('click', closeModalTeam);
+refs.modalTeamEl.addEventListener('click', onBackdropClick);
 
-function closeModalTeam () {
-  refs.modalTeamEl.classList.add ("is-hidden")
-  document.body.classList.remove("no-scroll");
+function closeModalTeam() {
+  refs.modalTeamEl.classList.add('is-hidden');
+  document.body.classList.remove('no-scroll');
 }
-function openModalTeam (e) {
-  e.preventDefault()
-  refs.modalTeamEl.classList.remove ("is-hidden")
+function openModalTeam(e) {
+  e.preventDefault();
+  refs.modalTeamEl.classList.remove('is-hidden');
   document.body.classList.add('no-scroll');
 }
 
-function onBackdropClick (e) {
-  console.log(e.target);
-  console.log(e.currentTarget);
+function onBackdropClick(e) {
   if (e.currentTarget === e.target) {
-     closeModalTeam();
-}}
-
-window.addEventListener ("keydown", (e) => {
-  if (e.code === "Escape") {
-    closeModalTeam()
+    closeModalTeam();
   }
-})
+}
 
-
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    closeModalTeam();
+  }
+});
