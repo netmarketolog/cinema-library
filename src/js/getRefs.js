c407866abb90cc41b4e3a54d05c706ab29a1af7c
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-09-30 08:41:58 +0300

    add new pagination and styles

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -3,13 +3,14 @@ export default function getRefs() {
     popularFilmsList: document.querySelector('.container__main'),
     spinner: document.querySelector('.spinner'),
     modalEl: document.querySelector('.backdrop-film'),
-    movieEl: document.querySelector(".card__item"),
+    movieEl: document.querySelector('.card__item'),
     watchedBtn: document.querySelector('[data-action="watched"]'),
     queueBtn: document.querySelector('[data-action="queue"]'),
     modalRendEl: document.querySelector('.film__container'),
     body: document.querySelector('body'),
-    modalTeamEl: document.querySelector ('.backdrop-team'),
-    modalTeamOpenBtn:document.querySelector ('.footer__team')
+    modalTeamEl: document.querySelector('.backdrop-team'),
+    modalTeamOpenBtn: document.querySelector('.footer__team'),
+    paginationEl: document.querySelector('.pagination ul'),
   };
   return refs;
 }
