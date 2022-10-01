d1526885a93c26459ed9f367924d55fd2c3b9a89
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-01 19:45:51 +0300

    LocalStorage
    Допиляно, функціонал
    Зміни в файлах addToLocalSt.js та modal-film.js
    Додано правильна перевірка та видалення з черги переглянутого фільму

--- src/js/local-storage/addToLStorage.js
+++ src/js/local-storage/addToLStorage.js
@@ -1,56 +1,32 @@
-// Імпорт
-// import getRefs from '../getRefs';
+ 
+const KEY_QUEUE = 'local';
 
-// Встановлення throttle
-// import throttle from 'lodash.throttle';
-
-// Слухачі подій
-// const refs = getRefs();
-// refs.addToQueueBtn.addEventListener('click', throttle(onQueueBtn, 500));
-// refs.addToWatchedBtn.addEventListener('click', throttle(onWatchedBtn, 500));
-
-// Константи
-// const KEY_QUEUE = 'queue';
-// const KEY_WATCHED = 'watched';
-
-let queueList = [];
-let watchedList = [];
+let local = {
+  queueList: [],
+  watchedList: [],
+};
 
 // Функція для роботи зі сховищем
 // Функція додавання фільмів до черги
 function onQueueBtn(film) {
-  const KEY_QUEUE = 'queue';
-  if (queueList.includes(film)) {
+  if (local.queueList.find(queueList => queueList.id === film.id)) {
     return;
   } else {
-    queueList.push(film);
+    local.queueList.push(film);
   }
-  localStorage.setItem(KEY_QUEUE, JSON.stringify(queueList));
+  localStorage.setItem(KEY_QUEUE, JSON.stringify(local));
 }
 // Функція додавання переглянутих фільмів
-function onWatchedBtn(film) {
-  const KEY_WATCHED = 'watched';
-  // const KEY_QUEUE = 'queue';
-  if (watchedList.includes(film)) {
-    return;
-  }
-  else {
-    // const saveQueueFilms = localStorage.getItem(KEY_QUEUE);
-    // const parseQueueFilms = JSON.parse(saveQueueFilms);
-    // // const queueFilmsID = parseQueueFilms.map(film.id)
-
-    // if (film === parseQueueFilms.includes(film)) {
-    // const indexFilm = parseQueueFilms.indexOf(film);
-    // const overwriting = parseQueueFilms.splice(indexFilm, 1);
-    // localStorage.setItem(KEY_QUEUE, JSON.stringify(overwriting));
-    
-    // }
-    watchedList.push(film);
-  }
-
-  localStorage.setItem(KEY_WATCHED, JSON.stringify(watchedList));
-  // console.log(parseQueueFilms);
-
+function onWatchedBtn(id) {
+  local = JSON.parse(localStorage.getItem(KEY_QUEUE));
+  local.queueList.map(film => {
+    if (id === film.id) {
+      local.watchedList.push(film);
+      const indexId = local.queueList.indexOf(film);
+      local.queueList.splice(indexId, 1);
+    }
+  });
+  localStorage.setItem(KEY_QUEUE, JSON.stringify(local));
 }
 
 export { onQueueBtn, onWatchedBtn };
