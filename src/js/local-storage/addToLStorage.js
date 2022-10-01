3412d2c87f1e15b873b4a98c65b29ee63dd851b3
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-01 20:21:46 +0300

    LocalStor
    Повернули два окремих об'єкта для зберігання фільмів
    Зміни в файлах addToLocalSt.js та modal-film.js
    Додано правильна перевірка та видалення з черги переглянутого фільму

--- src/js/local-storage/addToLStorage.js
+++ src/js/local-storage/addToLStorage.js
@@ -1,32 +1,32 @@
  
-const KEY_QUEUE = 'local';
+const KEY_QUEUE = 'queue';
+const KEY_WATCHED = 'watched';
 
-let local = {
-  queueList: [],
-  watchedList: [],
-};
+let queueList = [];
+let watchedList = [];
 
 // Функція для роботи зі сховищем
 // Функція додавання фільмів до черги
 function onQueueBtn(film) {
-  if (local.queueList.find(queueList => queueList.id === film.id)) {
+  if (queueList.find(queueList => queueList.id === film.id)) {
     return;
   } else {
-    local.queueList.push(film);
+    queueList.push(film);
   }
-  localStorage.setItem(KEY_QUEUE, JSON.stringify(local));
+  localStorage.setItem(KEY_QUEUE, JSON.stringify(queueList));
 }
 // Функція додавання переглянутих фільмів
 function onWatchedBtn(id) {
   local = JSON.parse(localStorage.getItem(KEY_QUEUE));
-  local.queueList.map(film => {
+  queueList.map(film => {
     if (id === film.id) {
-      local.watchedList.push(film);
-      const indexId = local.queueList.indexOf(film);
-      local.queueList.splice(indexId, 1);
+      watchedList.push(film);
+      const indexId = queueList.indexOf(film);
+      queueList.splice(indexId, 1);
     }
   });
-  localStorage.setItem(KEY_QUEUE, JSON.stringify(local));
+  localStorage.setItem(KEY_QUEUE, JSON.stringify(queueList));
+  localStorage.setItem(KEY_WATCHED, JSON.stringify(watchedList));
 }
 
 export { onQueueBtn, onWatchedBtn };
