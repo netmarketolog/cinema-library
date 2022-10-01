c840f336708f62f6ed1103c68eb6c6a8f43a46c2
Dmytro Soroka <dimasoroka17@gmail.com>
2022-10-01 13:45:12 +0300

    Library

--- src/js/local-storage/libraryStorage.js
+++ src/js/local-storage/libraryStorage.js
@@ -5,20 +5,6 @@ const refs = getRefs();
 refs.watchedBtn.addEventListener('click', onWatchedVideo);
 refs.queueBtn.addEventListener('click', onQueueVideo);
 
-function fetcMuvie(id) {
-    const API_KEY = '8fa17eefa9c2b424e1a30217c39bc412';
-    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
-    return fetch(url).then(response => {
-        return response.json();
-    })
-        .then(data => {
-            filmsMarkup(data)
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
-
 function onWatchedVideo() {
     refs.watchedBtn.setAttribute('disabled', true);
     refs.queueBtn.removeAttribute('disabled');
