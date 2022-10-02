bc20953dda26364bc46a69da7a4644cd5d2dbfc3
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-10-02 07:25:38 +0300

    none title

--- src/js/api/renderMarkup.js
+++ src/js/api/renderMarkup.js
@@ -19,7 +19,9 @@ export default function createPopularFilmsMarkup(films) {
             class="card__poster"
           />
 
-          <h2 class="card__title">${film.title}</h2>
+          <h2 class="card__title">${
+            film.title ? film.title : film.original_title
+          }</h2>
           <div class="card__wrap">
             <p class="card__description">${genres} | ${
         film.release_date ? film.release_date.slice(0, 4) : 'none'
