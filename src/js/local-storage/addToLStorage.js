c742ca635e7921de1d1685674c8c505e21926928
maxieo <97198812+maxieo@users.noreply.github.com>
2022-10-03 15:56:26 +0300

    фикс модалки на планшете и добавление текста на кнопку

--- src/js/local-storage/addToLStorage.js
+++ src/js/local-storage/addToLStorage.js
@@ -1,5 +1,9 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const refs = {
+  addToQueueBtn: document.querySelector("[data-addToQueue]"),
+  addToWatchedBtn: document.querySelector("[data-addToWatched]")
+}
 const KEY_QUEUE = 'queue';
 const KEY_WATCHED = 'watched';
 
@@ -26,6 +30,7 @@ function onQueueBtn(film) {
     });
     }
   }
+  changeTextQueue()
   localStorage.setItem(KEY_QUEUE, JSON.stringify(queueList));
 }
 // Функція додавання переглянутих фільмів
@@ -47,7 +52,23 @@ function onWatchedBtn(film) {
       });
     }
   }
+  changeTextWatched()
   localStorage.setItem(KEY_WATCHED, JSON.stringify(watchedList));
+}
+
+//изменение текста кнопок
+
+function changeTextWatched () {
+  if (onWatchedBtn) {
+    refs.addToWatchedBtn.textContent = 'Adding to Watched successful'
+
+  }
+}
+
+function changeTextQueue () {
+  if (onQueueBtn) {
+    refs.addToQueueBtn.textContent = 'Adding to queue successful'
+  }
 }
 
 export { onQueueBtn, onWatchedBtn };
